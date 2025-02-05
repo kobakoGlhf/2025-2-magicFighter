7c2333a0078a0875aa9f2e9148e8fe0afe7ac21f
kobakoGlhf <147145211+kobakoGlhf@users.noreply.github.com>
2025-02-06 01:01:29 +0900

    [add]IK導入

---
+++ Assets/Script/Character/CharactorAnimationManager.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MFFrameWork
+{
+    public class CharactorAnimation
+    {
+        static readonly Dictionary<AnimationKind, string> ClipName = new Dictionary<AnimationKind, string>()
+    {
+        {AnimationKind.Move, "Move" },
+        {AnimationKind.Jump, "Jump" },
+        {AnimationKind.Attack, "Attack" }
+    };
+        static readonly Dictionary<AnimationPropertys, string> PropertysName = new Dictionary<AnimationPropertys, string>()
+    {
+        {AnimationPropertys.MoveSpeed,"MoveSpeed" },
+        {AnimationPropertys.IsGround, "IsGround" },
+        {AnimationPropertys.AttackTrigger,"AttackTrigger" },
+        {AnimationPropertys.DamageTrigger,"DamageTrigger" },
+        {AnimationPropertys.JumpTrigger,"JumpTrigger" },
+        {AnimationPropertys.DushTrigger,"DushTrigger" },
+    };
+
+        Animator _animator;
+        public void SetAnimator(Animator animator) => _animator = animator;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="kind"></param>
+        /// <param name="changeMod"></param>
+        /// <param name="duration">アニメーションの遷移時間。ChangeModeがCrossFadeの時のみ使用される</param>
+        public void AnimationChange(AnimationKind kind, ChangeAnimation changeMod, float duration = 1)
+        {
+            switch (changeMod)
+            {
+                case ChangeAnimation.Defalt:
+                    _animator.Play(ClipName[kind]);
+                    break;
+                case ChangeAnimation.CrossFade:
+                    _animator.CrossFade(ClipName[kind], duration);
+                    break;
+            }
+        }
+        public void SetFloat(AnimationPropertys kind, float value)
+        {
+            _animator.SetFloat(PropertysName[kind], value);
+        }
+        public void SetTrigger(AnimationPropertys kind)
+        {
+            _animator.SetTrigger(PropertysName[kind]);
+        }
+        public void SetBool(AnimationPropertys kind, bool frag)
+        {
+            _animator.SetBool(PropertysName[kind], frag);
+        }
+    }
+    public enum ChangeAnimation
+    {
+        Defalt,
+        CrossFade
+    }
+    public enum AnimationKind
+    {
+        Move,
+        Jump,
+        Attack
+    }
+    public enum AnimationPropertys
+    {
+        MoveSpeed,
+        IsGround,
+
+        AttackTrigger,
+        DamageTrigger,
+        JumpTrigger,
+        DushTrigger
+    }
+}
---
+++ Assets/Script/Character/IKAnimation.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+namespace MFFrameWork
+{
+    public class IKAnimation: MonoBehaviour
+    {
+        Animator _animator;
+        [SerializeField]
+        LayerMask _layerMask;
+        [SerializeField]
+        string _groundTagName;
+        [SerializeField, Range(0, 1f)]
+        float _groundDistanse;
+        void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        private void OnAnimatorIK(int layerIndex)
+        {
+            if (_animator == null) Debug.Log("Aniator is null");
+            _animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1f);
+            _animator.SetIKRotationWeight(AvatarIKGoal.LeftFoot, 1f);
+            _animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1f);
+            _animator.SetIKRotationWeight(AvatarIKGoal.RightFoot, 1f);
+            //ë´Ç™ínñ Ç…ñÑÇ‹Ç¡ÇƒÇ¢ÇÈèÍçáÇÃëŒçÙÇ∆ÇµÇƒè„ï˚å¸Ç…Ç∏ÇÁÇµÇƒRayÇê∂ê¨Ç∑ÇÈ
+            Ray ray;
+            RaycastHit hit;
+
+            ray = new Ray(_animator.GetIKPosition(AvatarIKGoal.LeftFoot) + Vector3.up, Vector3.down);
+            if (Physics.Raycast(ray, out hit, _groundDistanse + 1, _layerMask))
+            {
+                if (hit.transform.tag == _groundTagName)
+                {
+                    Vector3 footPos = hit.point;
+                    footPos.y += _groundDistanse;
+                    Debug.Log(footPos);
+                    _animator.SetIKPosition(AvatarIKGoal.LeftFoot, footPos);
+                    //_animator.SetIKRotation(AvatarIKGoal.LeftFoot, _animator.GetIKRotation(AvatarIKGoal.LeftFoot) * Quaternion.FromToRotation(Vector3.forward, hit.normal));
+                }
+            }
+            ray = new Ray(_animator.GetIKPosition(AvatarIKGoal.RightFoot) + Vector3.up, Vector3.down);
+            if (Physics.Raycast(ray, out hit, _groundDistanse + 1, _layerMask))
+            {
+                if (hit.transform.tag == _groundTagName)
+                {
+                    Vector3 footPos = hit.point;
+                    footPos.y += _groundDistanse;
+                    Debug.Log(footPos);
+                    _animator.SetIKPosition(AvatarIKGoal.RightFoot, footPos);
+                    //_animator.SetIKRotation(AvatarIKGoal.LeftFoot, _animator.GetIKRotation(AvatarIKGoal.LeftFoot) * Quaternion.FromToRotation(Vector3.forward, hit.normal));
+                }
+            }
+        }
+    }
+}
