6d2160cc348f9fc306cddad23e6c9841237f4af2
kobakoGlhf <147145211+kobakoGlhf@users.noreply.github.com>
2025-02-07 05:55:58 +0900

    [update]missile関係実装

--- Assets/Script/Character/Weapon/Missile.cs
+++ Assets/Script/Character/Weapon/Missile.cs
@@ -3,33 +3,80 @@ using UnityEngine;
 namespace MFFrameWork
 {
     [RequireComponent(typeof(Rigidbody))]
-    public class Missile : MonoBehaviour
+    public class Missile : MonoBehaviour, IBullet
     {
         [SerializeField] Transform _target;
         [SerializeField] float _speed;
+
+        [Space]
+        [SerializeField] Vector3 _accleration;
+        [SerializeField] float period = 3;
+        [SerializeField] float _maxAccleration;
+        Vector3 _velocity;
+        Vector3 _position;
+
         [SerializeField] GameObject _effect;
-        float _damage;
+        float _hitDamage;
         Rigidbody _rb;
+        LayerMask _ignoreLayer;
 
         private void Start()
         {
             _rb = GetComponent<Rigidbody>();
         }
-        void Init(Transform target, float speed, GameObject effect, float damage)
+        void IBullet.Init(Transform target, float attackPower, LayerMask ignoreLayer)
         {
             _target = target;
-            _speed = speed;
-            _effect = effect;
-            _damage = damage;
+            _hitDamage = attackPower;
+            _ignoreLayer = ignoreLayer;
+            _position = transform.position;
         }
 
-        // Update is called once per frame
         void Update()
         {
-            Vector3 targetVector = _target.position - transform.position;
-            targetVector.Normalize();
+            //‰^“®•û’öŽ®‚ðŽg‚Á‚½missile
 
-            _rb.linearVelocity = targetVector * _speed;
+            var accleration = _accleration;
+
+            var diff = _target.position - transform.position;
+            accleration += (diff - _velocity * period) * 2 / (period * period);
+
+            if (accleration.magnitude > 100f)
+            {
+                accleration = accleration.normalized * 100f;
+            }
+
+            period -= Time.deltaTime;
+            if (period < 0)
+            {
+                return;
+            }
+
+            _velocity += accleration * Time.deltaTime;
+            _position += _velocity * Time.deltaTime;
+            transform.position = _position;
+            //    Vector3 targetVector = _target.position - transform.position;
+            //    targetVector.Normalize();
+
+            //    _rb.linearVelocity = targetVector * _speed;
         }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.layer == _ignoreLayer ||
+                other.gameObject.layer == gameObject.layer) return;
+
+            if (other.TryGetComponent(out IDamageable damageable))
+            {
+                damageable.Damage(_hitDamage, this.transform);
+            }
+            DethBehaviour();
+            Destroy(this.gameObject);
+        }
+        void DethBehaviour()
+        {
+            Debug.Log("‚±‚±‚Å”š”­");
+        }
+
     }
 }
--- Assets/Script/Character/Weapon/Weapon_B.cs
+++ Assets/Script/Character/Weapon/Weapon_B.cs
@@ -4,11 +4,7 @@ namespace MFFrameWork
 {
     public abstract class Weapon_B : MonoBehaviour, IAttack
     {
-        [SerializeField] int _attackPower;
-        [SerializeField] Transform _targetPos;
-        int IAttack.AttackPower { get => _attackPower; set => _attackPower=value; }
-
-        void IAttack.OnAttack(float attackPower, bool cancel)
+        public virtual void OnAttack(Transform _targetPos, float attackPower, bool cancel)
         {
             throw new System.NotImplementedException();
         }
